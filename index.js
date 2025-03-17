593e10052b603a2f377d4622a2675db6ce51712b
Zakhar-38 <28zaxarka@mail.ru>
2025-03-17 14:51:58 +0300

    zadanie2

--- index.js
+++ index.js
@@ -20,30 +20,9 @@ const User = sequelize.define('users', {
     password: { type: DataTypes.STRING },
     role: { type: DataTypes.STRING, defaultValue: "user" },
 })
-
-app.get('/',(req,res) => {
-res.send('<h1>я сделаю 1 задание</h1>')
-})
-
 app.get('/fio',(req,res) => {
     res.send('<h1>Крутоголов Александр, группа 11ИС - 322. </h1>')
     })
-
-    app.post('/Create_User', (req, res) => {
-        const { id, login, password, role } = req.body
-        const type = User.create({
-            id, login, password, role
-        });
-        return res.json(type);
-    })
-    
-    app.delete('/Delete_User/Delete_ID/:Delete_ID', (req, res) => {
-        const { id, login, password, role } = req.body
-        let Delete_ID = Number(req.params.Delete_ID)
-        const type = User.destroy({ where: { id: Delete_ID } });
-        res.send("Ну удалил и че")
-    })
-
 async function start() {
     app.listen(PORT, () => {
         console.log(`address http://localhost:${PORT}`)

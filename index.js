2d7666de3f8040d932f6bd4ff2e6252fa6f26df0
Zakhar-38 <28zaxarka@mail.ru>
2025-03-17 13:57:07 +0300

    add-web server

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-pidor
+const express = require('express');
+require("dotenv").config()
+const app = express();
+const { DataTypes } = require('sequelize')
+const PORT = 8080
+const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize(
+    "Baza",
+    "postgres",
+    "8777",
+    {
+        dialect: 'postgres',
+        host: "localhost",
+        port: "5432"
+    }
+)
+const User = sequelize.define('users', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    login: { type: DataTypes.STRING, unique: true },
+    password: { type: DataTypes.STRING },
+    role: { type: DataTypes.STRING, defaultValue: "user" },
+})
+app.get('/',(req,res) => {
+res.send('<h1>я сделаю 1 задание</h1>')
+})
+
+
+async function start() {
+    app.listen(PORT, () => {
+        console.log(`address http://localhost:${PORT}`)
+    })
+}
+sequelize.authenticate();
+sequelize.sync();
+start()
